611772f19b362309150580abd359f46779f19b0f
nasc1mento <anr@discente.ifpe.edu.br>
2024-09-13 09:27:53 -0300

    feat: restricting fields

--- SocketChat/app/src/main/java/br/com/socketchat/CriarUsuarioActivity.java
+++ SocketChat/app/src/main/java/br/com/socketchat/CriarUsuarioActivity.java
@@ -4,13 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.Button;
 import android.widget.EditText;
 
 import br.com.socketchat.model.User;
 import br.com.socketchat.repository.UserRepository;
 
-public class CriarUsuarioActivity extends AppCompatActivity {
+public class CriarUsuarioActivity extends AppCompatActivity implements TextWatcher {
 
     private EditText etCriarUsuario;
     private Button btnSalvar;
@@ -29,6 +31,7 @@ public class CriarUsuarioActivity extends AppCompatActivity {
     private void initViews(){
         etCriarUsuario = findViewById(R.id.et_criarUsuario);
         btnSalvar = findViewById(R.id.btn_salvar);
+        etCriarUsuario.addTextChangedListener(this);
     }
 
     private void salvarUsuario(){
@@ -43,4 +46,23 @@ public class CriarUsuarioActivity extends AppCompatActivity {
         finish();
     }
 
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if (s.toString().isEmpty()) {
+            etCriarUsuario.setError("Mínimo 1 caractere");
+            btnSalvar.setEnabled(false);
+        } else {
+            btnSalvar.setEnabled(true);
+        }
+    }
 }
--- SocketChat/app/src/main/java/br/com/socketchat/ProfileActivity.java
+++ SocketChat/app/src/main/java/br/com/socketchat/ProfileActivity.java
@@ -3,6 +3,8 @@ package br.com.socketchat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 
@@ -11,7 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import br.com.socketchat.model.User;
 import br.com.socketchat.repository.UserRepository;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity implements TextWatcher {
 
     private UserRepository userRepository;
     private EditText et;
@@ -30,6 +32,7 @@ public class ProfileActivity extends AppCompatActivity {
     private void initViews() {
         et = findViewById(R.id.et_updateUser);
         et.setText(getIntent().getStringExtra("name"));
+        et.addTextChangedListener(this);
     }
 
     private void initSaveBtn() {
@@ -54,4 +57,23 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        if (s.toString().isEmpty()) {
+            et.setError("Mínimo 1 caractere");
+            findViewById(R.id.save_profile_btn).setEnabled(false);
+        } else {
+            findViewById(R.id.save_profile_btn).setEnabled(true);
+        }
+    }
 }
