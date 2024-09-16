4db8413c910533d3b1683e687ba29026dd447a83
nasc1mento <anr@discente.ifpe.edu.br>
2024-09-16 02:16:11 -0300

    feat: join event

--- SocketServer/server.js
+++ SocketServer/server.js
@@ -1,26 +1,60 @@
 const WebSocket = require("ws");
+const { v4: uuidv4 } = require('uuid');
 const server = new WebSocket.Server({
     port: 3000
 });
 
-let connections = []
+const connections = [];
+
 
 server.on("connection", connection => {
-    console.log("new connection");
-    connections.push(connection);
-
     connection.on("message", message => {
-        connections.forEach(element => {
-            if(element != connection)
-                element.send(message.toString("utf-8"));
-        });
+        const parsed = JSON.parse(message);
+
+        if (parsed.type == "join") {
+            connection.id = uuidv4();
+            connection.name = parsed.name;
+            connections.push(connection);
+            
+            // server.sendBroadcast(JSON.stringify({
+            //     "list": connections.map(c => ({ id: c.id, name: c.name }))
+            // }));
+
+        // } else if(parsed.type == "connections") {
+        //     connection.send(JSON.stringify({
+        //         "list": connections.map(c => ({ id: c.id, name: c.name }))
+        //     }));
+
+        } else {
+            server.sendBroadcastEx(connection, JSON.stringify(parsed).toString("utf-8"));
+        }
     });
+
 
     connection.on("close", () => {
-        console.log("connection closed");
         connections.splice(connections.indexOf(connection), 1);
+
+        // server.sendBroadcast(JSON.stringify({
+        //     "type": "left",
+        //     "list": connections.map(c => ({ id: c.id, name: c.name }))
+        // }));
+        
     });
 })
+
+
+server.sendBroadcast = data => {
+    connections.forEach(element => {
+        element.send(data);
+    });
+}
+
+server.sendBroadcastEx = (sender, data) => {
+    connections.forEach(element => {
+        if (element != sender)
+            element.send(data);
+    });
+}
 
 
 
